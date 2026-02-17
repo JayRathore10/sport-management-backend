e344cc3e181a5d1176d79c8c2164480b18cf5043
Jay <jayrathore88155@gmail.com>
2026-02-17 19:35:14 +0530

    Minor change in player Model

--- src/models/player.model.ts
+++ src/models/player.model.ts
@@ -2,33 +2,41 @@ import mongoose from "mongoose";
 
 const playerSchema = new mongoose.Schema({
   name: {
-    type : String , 
-    minLength : 3 , 
-    maxLength : 30 ,
-    required : [true , "Name is req"]
-  } , 
-  sport : {
-    type : String ,
-    maxLength : 20 , 
-    required : [true, "Sport name is needed"]
-  } , 
-  mobileNumber : {
-    type : String , 
-    minLength : 10 , 
-    maxLength : 10 , 
-    required : [true , "Mobile Number Needed"]
-  } , 
-  age : {
-    type : Number , 
-    required : [true , "Age is needed"]
-  } ,
-  gender :{
-    type : String , 
-    enum : ["Male" , "Female"],
-    required : [true , "Gender is needed"]
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    required: [true, "Name is req"]
+  },
+  sport: {
+    type: String,
+    maxLength: 20,
+    required: [true, "Sport name is needed"]
+  },
+  mobileNumber: {
+    type: String,
+    minLength: 10,
+    maxLength: 10,
+    required: [true, "Mobile Number Needed"]
+  },
+  age: {
+    type: Number,
+    required: [true, "Age is needed"]
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female"],
+    required: [true, "Gender is needed"]
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, 'Please fill a valid email address']
   }
 })
 
-const Player = mongoose.model("player" , playerSchema);
+const Player = mongoose.model("player", playerSchema);
 
 export default Player;
